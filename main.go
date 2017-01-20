97eb16861ca6f8c013242719a5952b16b1075e99
Alexander F Rødseth <rodseth@gmail.com>
2017-01-20 23:52:26 +0100

    Format the output differently

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net"
@@ -10,6 +11,18 @@ import (
 
 	"github.com/xyproto/term"
 )
+
+var (
+	noHighlightPrefixes = []string{"vbox", "docker", "lo"}
+)
+
+func pad(s string, n int) string {
+	var padding string
+	for i := 0; i < (n - len(s)); i++ {
+		padding += " "
+	}
+	return s + padding
+}
 
 func main() {
 	enableColors := runtime.GOOS != "windows"
@@ -21,16 +34,37 @@ func main() {
 	}
 
 	for _, iface := range ifaces {
-		fmt.Printf("%s%s%s ", o.DarkGray("["), o.LightBlue(strconv.Itoa(iface.Index)), o.DarkGray("]"))
-		if strings.HasPrefix(iface.Name, "vbox") || strings.HasPrefix(iface.Name, "docker") || strings.HasPrefix(iface.Name, "lo") {
-			fmt.Print(o.LightGreen(iface.Name))
-		} else {
-			fmt.Print(o.White(iface.Name))
+		var w bytes.Buffer
+		fmt.Fprintf(&w, "%s%s%s ", o.DarkGray("["), o.LightBlue(strconv.Itoa(iface.Index)), o.DarkGray("]"))
+
+		highlight := true
+		for _, noh := range noHighlightPrefixes {
+			if strings.HasPrefix(iface.Name, noh) {
+				highlight = false
+				break
+			}
 		}
-		fmt.Printf(" %s", o.DarkGray(iface.Flags.String()))
-		fmt.Printf("\t%s", o.LightYellow(iface.HardwareAddr.String()))
-		fmt.Printf("\t%s", o.DarkPurple("MTU "+strconv.Itoa(iface.MTU)))
-		fmt.Println()
+
+		paddedName := pad(iface.Name, 8)
+
+		if highlight {
+			fmt.Fprintf(&w, o.White(paddedName))
+		} else {
+			fmt.Fprintf(&w, o.LightGreen(paddedName))
+		}
+
+		hwAddr := iface.HardwareAddr.String()
+		if hwAddr == "" {
+			fmt.Fprintf(&w, "\t\t%s", o.LightYellow(pad("-", 17)))
+		} else {
+			fmt.Fprintf(&w, "\t\t%s", o.LightYellow(hwAddr))
+		}
+
+		fmt.Fprintf(&w, "  %s", o.DarkPurple("MTU "+strconv.Itoa(iface.MTU)))
+		fmt.Fprintf(&w, "  %s", o.DarkGray(iface.Flags.String()))
+
+		fmt.Println(w.String())
+		w = bytes.Buffer{}
 
 		addrs, err := iface.Addrs()
 		if err != nil {
@@ -43,17 +77,19 @@ func main() {
 				parts := strings.Split(adrstr, "/")
 				adrstr = strings.Replace(adrstr, parts[0], o.DarkRed(parts[0]), -1)
 			}
-			fmt.Printf("\t%s\t%s %s\n", o.LightBlue("adr"), o.White(adrstr), o.DarkGray("(")+a.Network()+o.DarkGray(")"))
+			fmt.Fprintf(&w, "  %s\t%s\t%s\n", o.LightBlue("adr"), o.White(pad(adrstr, 32+11)), o.DarkGray("(")+a.Network()+o.DarkGray(")"))
 		}
 
-		maddrs, err := iface.MulticastAddrs()
+		mAddrs, err := iface.MulticastAddrs()
 		if err != nil {
 			log.Println(err)
 			continue
 		}
-		for _, ma := range maddrs {
-			fmt.Printf("\t%s\t%s %s\n", o.LightPurple("mul"), o.DarkBlue(ma.String()), o.DarkGray("(")+ma.Network()+o.DarkGray(")"))
+		for _, ma := range mAddrs {
+			fmt.Fprintf(&w, "  %s\t%s\t%s\n", o.LightPurple("mul"), o.DarkBlue(pad(ma.String(), 32)), o.DarkGray("(")+ma.Network()+o.DarkGray(")"))
 		}
+
+		fmt.Println(w.String())
 
 	}
 }
